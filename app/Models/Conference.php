8afbc8f0defadadbb89496ab10f94786fc0cc988
Htet Wai Yan Aung <waiyan@andfun.biz>
2024-04-22 23:48:21 +0700

    Update layout for conference

--- app/Models/Conference.php
+++ app/Models/Conference.php
@@ -5,9 +5,12 @@ namespace App\Models;
 use App\Enums\Region;
 use Filament\Forms\Components\CheckboxList;
 use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\Fieldset;
+use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
 use Filament\Forms\Get;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -41,80 +44,101 @@ class Conference extends Model
     public static function getForm()
     {
         return [
-            TextInput::make('name')
-                ->label('Conference')
-                ->required()
-                ->maxLength(60),
-            Textarea::make('description')
-                ->label('What is the conference about?')
-                ->required(),
-            DateTimePicker::make('start_date')
-                ->native(false)
-                ->default(now())
-                ->closeOnDateSelection()
-                ->required(),
-            DateTimePicker::make('end_date')
-                ->native(false)
-                ->closeOnDateSelection()
-                ->required(),
-            Select::make('status')
-                ->required()
-                ->options([
-                    'finished' => 'Finished',
-                    'waiting' => 'Waiting',
-                    'current' => 'Current'
-                ]),
-            Select::make('timezone')
-                ->required()
-                ->options([
-                    "utc" => "UTC",
-                    "gmt" => "GMT",
-                    "bst" => "BST",
-                    "cet" => "CET",
-                    "cest" => "CEST",
-                    "eet" => "EET",
-                    "eest" => "EEST",
-                    "est" => "EST",
-                    "edt" => "EDT",
-                    "cst" => "CST",
-                    "cdt" => "CDT",
-                    "mst" => "MST",
-                    "mdt" => "MDT",
-                    "pst" => "PST",
-                    "pdt" => "PDT",
-                ]),
-            Select::make('region')
-                ->live()
-                ->enum(Region::class)
-                ->options(Region::class),
-            Select::make('venue_id')
-                ->searchable()
-                ->preload()
-                ->relationship('venue', 'name', modifyQueryUsing: function(Builder $query, Get $get) {
-                    return $query->where('region',$get('region'));
-                })
-                ->createOptionForm(
-                    function(Get $get) {
-                        return [
-                            TextInput::make('name')
+            Section::make('Conference Details')
+                ->collapsible()
+                ->description('Provide information about the conference')
+                ->columns(2)
+                ->schema([
+                    TextInput::make('name')
+                        ->columnSpanFull()
+                        ->label('Conference')
+                        ->required()
+                        ->maxLength(60),
+                    Textarea::make('description')
+                        ->columnSpanFull()
+                        ->label('What is the conference about?')
+                        ->required(),
+                    DateTimePicker::make('start_date')
+                        ->native(false)
+                        ->default(now())
+                        ->closeOnDateSelection()
+                        ->required(),
+                    DateTimePicker::make('end_date')
+                        ->native(false)
+                        ->closeOnDateSelection()
+                        ->required(),
+                    Fieldset::make('Status')
+                        ->schema([
+                            Toggle::make('is_published')
+                                ->label('Is publish?'),
+                            Select::make('status')
                                 ->required()
-                                ->maxLength(255),
-                            TextInput::make('city')
-                                ->required()
-                                ->maxLength(255),
-                            TextInput::make('postal_code')
-                                ->required()
-                                ->maxLength(255),
-                            Select::make('region')
-                                ->enum(Region::class)
-                                ->options(Region::class)
-                                ->default($get('region'))
-                        ];
-                    }
-                ),
-            CheckboxList::make('speakers')
-                ->relationship('speakers','name')
-                ->options(Speaker::all()->pluck('name','id'))
+                                ->options([
+                                    'finished' => 'Finished',
+                                    'waiting' => 'Waiting',
+                                    'current' => 'Current'
+                                ]),
+                        ]),
+                    
+            ]),
+            Section::make('Time and Location')
+                ->schema([
+                    Select::make('timezone')
+                        ->required()
+                        ->options([
+                            "utc" => "UTC",
+                            "gmt" => "GMT",
+                            "bst" => "BST",
+                            "cet" => "CET",
+                            "cest" => "CEST",
+                            "eet" => "EET",
+                            "eest" => "EEST",
+                            "est" => "EST",
+                            "edt" => "EDT",
+                            "cst" => "CST",
+                            "cdt" => "CDT",
+                            "mst" => "MST",
+                            "mdt" => "MDT",
+                            "pst" => "PST",
+                            "pdt" => "PDT",
+                        ]),
+                    Select::make('region')
+                        ->live()
+                        ->enum(Region::class)
+                        ->options(Region::class),
+                    Select::make('venue_id')
+                        ->searchable()
+                        ->preload()
+                        ->relationship('venue', 'name', modifyQueryUsing: function(Builder $query, Get $get) {
+                            return $query->where('region',$get('region'));
+                        })
+                        ->createOptionForm(
+                            function(Get $get) {
+                                return [
+                                    TextInput::make('name')
+                                        ->required()
+                                        ->maxLength(255),
+                                    TextInput::make('city')
+                                        ->required()
+                                        ->maxLength(255),
+                                    TextInput::make('postal_code')
+                                        ->required()
+                                        ->maxLength(255),
+                                    Select::make('region')
+                                        ->enum(Region::class)
+                                        ->options(Region::class)
+                                        ->default($get('region'))
+                                ];
+                            }
+                        ),
+                    
+                 ]),
+            Section::make('Speaker Information')
+                ->schema([
+                    CheckboxList::make('speakers')
+                    ->relationship('speakers','name')
+                    ->options(Speaker::all()->pluck('name','id'))
+                ])
         ];
     }
 }
