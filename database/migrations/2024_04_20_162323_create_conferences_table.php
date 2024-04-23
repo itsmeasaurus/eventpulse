30209ca8325c946b23d2374b6c4cf208027ee3c0
Htet Wai Yan Aung <waiyan@andfun.biz>
2024-04-23 23:06:08 +0700

    Update conference venue_id column

--- database/migrations/2024_04_20_162323_create_conferences_table.php
+++ database/migrations/2024_04_20_162323_create_conferences_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->string('status');
             $table->string('timezone');
             $table->string('region');
-            $table->foreignId('venue_id');
+            $table->foreignId('venue_id')->nullable();
             $table->timestamps();
         });
     }
